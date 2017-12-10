9414625fffe8b683caa4dd486c5c773403ebd97a
Pit Kleyersburg <pitkley@googlemail.com>
2017-12-10 13:19:10 +0100

    Don't allocate a pseudo-TTY
    This fixes issue #52 as far as I can tell.
    
    Since documentation on the `-t` flag is rather sparse, I am not sure if
    this has any negative implications on existing usage of cross.

--- src/docker.rs
+++ src/docker.rs
@@ -62,7 +62,7 @@ pub fn register(target: &Target, verbose: bool) -> Result<()> {
     docker_command("run")
         .arg("--privileged")
         .arg("--rm")
-        .arg("-it")
+        .arg("-i")
         .arg("ubuntu:16.04")
         .args(&["sh", "-c", cmd])
         .run(verbose)
@@ -159,7 +159,7 @@ pub fn run(target: &Target,
         .args(&["-v", &format!("{}:/rust:ro", sysroot.display())])
         .args(&["-v", &format!("{}:/target", target_dir.display())])
         .args(&["-w", "/project"])
-        .args(&["-it", &image(toml, target)?])
+        .args(&["-i", &image(toml, target)?])
         .args(&["sh", "-c", &format!("PATH=$PATH:/rust/bin {:?}", cmd)])
         .run_and_get_status(verbose)
 }
